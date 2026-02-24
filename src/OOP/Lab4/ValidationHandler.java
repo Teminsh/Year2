9bc1a0904b269de461977d25ffc65e5dee419383
Teminsh <135225409+Teminsh@users.noreply.github.com>
2026-02-24 16:52:42 +0200

    OOP Lab 4 finally final

--- src/OOP/Lab4/ValidationHandler.java
+++ src/OOP/Lab4/ValidationHandler.java
@@ -23,3 +23,32 @@ class PositiveNumberValidator implements EventHandler<PropertyChangingEventArgs>
         }
     }
 }
+
+class RangeValidator implements EventHandler<PropertyChangingEventArgs> {
+    private final String targetProperty;
+    private final double minValue;
+    private final double maxValue;
+
+    public RangeValidator (String targetProperty, double minValue, double maxValue) {
+        this.targetProperty = targetProperty;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
+
+    @Override
+    public void handle(Object sender, PropertyChangingEventArgs args)
+    {
+        if (!targetProperty.equals(args.getPropertyName())) return;
+
+        if (args.getNewValue() instanceof Number n) {
+            double value = n.doubleValue();
+            if (value < minValue || value > maxValue) {
+                System.out.println("[VALIDATION] Отклонено: '" + args.getPropertyName() +
+                        "' должно быть в [" + minValue + ", " + maxValue +
+                        "], а не " + value);
+                args.setCanChange(false);
+            }
+        }
+    }
+
+}
